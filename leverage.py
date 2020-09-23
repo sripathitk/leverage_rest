8ec59b1343d566bfe5065df227e12c3b5f82cb6a
BELLVM35\Administrator <sripathi_for@yahoo.co.in>
2020-09-23 17:16:35 +0530

    minor changes

--- leverage.py
+++ leverage.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from flask import request
 
 import logging
@@ -9,68 +9,67 @@ import os
 
 logging.basicConfig(level=logging.DEBUG)
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/placeLev",methods=['POST'])
+
+@app.route("/placeLev", methods=['POST'])
 def placelev():
+    with open(os.path.join(os.getcwd(), 'config/UserConfig.json')) as f:
+        users_d = json.load(f)
 
-       with open(os.path.join(os.getcwd(), 'config/UserConfig.json')) as f:
-              users_d = json.load(f)
+    username = users_d['users']['suma']['aliceblue']['username']
+    password = users_d['users']['suma']['aliceblue']['password']
+    twofa = users_d['users']['suma']['aliceblue']['twoFA']
+    api_secret = users_d['users']['suma']['aliceblue']['api_secret']
+    access_token = users_d['users']['suma']['aliceblue']['access_token']
+    logging.info('access token is {}'.format(access_token))
+    modified_date = users_d['users']['suma']['aliceblue']['modified_date']
+    # BNF parameters
+    bnf_enabled = users_d['users']['suma']['lev_bnf']['enabled']
+    bnf_trading_symbol = users_d['users']['suma']['lev_bnf']['trading_symbol']
+    bnf_lot_size = int(users_d['users']['suma']['lev_bnf']['lot_size'])
+    bnf_ref_trade_size = int(users_d['users']['suma']['lev_bnf']['ref_trade_size'])
+    bnf_fno_exchange = users_d['users']['suma']['lev_bnf']['fno_exchange']
+    bnf_ce_position_type = users_d['users']['suma']['lev_bnf']['cePositionType']
+    bnf_pe_position_type = users_d['users']['suma']['lev_bnf']['pePositionType']
+    bnf_leverage_quantity = int(users_d['users']['suma']['lev_bnf']['leverageQuantity'])
+    bnf_lev_order_type = users_d['users']['suma']['lev_bnf']['lev_order_type']
+    bnf_exchange = users_d['users']['suma']['lev_bnf']['exchange']
+    bnf_slm_order_type = users_d['users']['suma']['lev_bnf']['slmOrderType']
+    bnf_ref_pos_quantity = users_d['users']['suma']['lev_bnf']['ref_pos_quantity']
+    # NF parameters
+    nf_enabled = users_d['users']['suma']['lev_nf']['enabled']
+    nf_trading_symbol = users_d['users']['suma']['lev_nf']['trading_symbol']
+    nf_non_trading_symbol = users_d['users']['suma']['lev_nf']['non_trading_symbol']
+    nf_lot_size = int(users_d['users']['suma']['lev_nf']['lot_size'])
+    nf_ref_trade_size = int(users_d['users']['suma']['lev_nf']['ref_trade_size'])
+    nf_fno_exchange = users_d['users']['suma']['lev_nf']['fno_exchange']
+    nf_ce_position_type = users_d['users']['suma']['lev_nf']['cePositionType']
+    nf_pe_position_type = users_d['users']['suma']['lev_nf']['pePositionType']
+    nf_leverage_quantity = int(users_d['users']['suma']['lev_nf']['leverageQuantity'])
+    nf_lev_order_type = users_d['users']['suma']['lev_nf']['lev_order_type']
+    nf_exchange = users_d['users']['suma']['lev_nf']['exchange']
+    nf_slm_order_type = users_d['users']['suma']['lev_nf']['slmOrderType']
+    nf_ref_pos_quantity = users_d['users']['suma']['lev_nf']['ref_pos_quantity']
+    # code block to figure out whether access token is available or not.
 
-       username = users_d['users']['suma']['aliceblue']['username']
-       password = users_d['users']['suma']['aliceblue']['password']
-       twofa = users_d['users']['suma']['aliceblue']['twoFA']
-       api_secret = users_d['users']['suma']['aliceblue']['api_secret']
-       access_token = users_d['users']['suma']['aliceblue']['access_token']
-       logging.info('access token is {}'.format(access_token))
-       modified_date = users_d['users']['suma']['aliceblue']['modified_date']
-       # BNF parameters
-       bnf_enabled = users_d['users']['suma']['lev_bnf']['enabled']
-       bnf_trading_symbol = users_d['users']['suma']['lev_bnf']['trading_symbol']
-       bnf_lot_size = int(users_d['users']['suma']['lev_bnf']['lot_size'])
-       bnf_ref_trade_size = int(users_d['users']['suma']['lev_bnf']['ref_trade_size'])
-       bnf_fno_exchange = users_d['users']['suma']['lev_bnf']['fno_exchange']
-       bnf_ce_position_type = users_d['users']['suma']['lev_bnf']['cePositionType']
-       bnf_pe_position_type = users_d['users']['suma']['lev_bnf']['pePositionType']
-       bnf_leverage_quantity = int(users_d['users']['suma']['lev_bnf']['leverageQuantity'])
-       bnf_lev_order_type = users_d['users']['suma']['lev_bnf']['lev_order_type']
-       bnf_exchange = users_d['users']['suma']['lev_bnf']['exchange']
-       bnf_slm_order_type = users_d['users']['suma']['lev_bnf']['slmOrderType']
-       bnf_ref_pos_quantity = users_d['users']['suma']['lev_bnf']['ref_pos_quantity']
-       # NF parameters
-       nf_enabled = users_d['users']['suma']['lev_nf']['enabled']
-       nf_trading_symbol = users_d['users']['suma']['lev_nf']['trading_symbol']
-       nf_non_trading_symbol = users_d['users']['suma']['lev_nf']['non_trading_symbol']
-       nf_lot_size = int(users_d['users']['suma']['lev_nf']['lot_size'])
-       nf_ref_trade_size = int(users_d['users']['suma']['lev_nf']['ref_trade_size'])
-       nf_fno_exchange = users_d['users']['suma']['lev_nf']['fno_exchange']
-       nf_ce_position_type = users_d['users']['suma']['lev_nf']['cePositionType']
-       nf_pe_position_type = users_d['users']['suma']['lev_nf']['pePositionType']
-       nf_leverage_quantity = int(users_d['users']['suma']['lev_nf']['leverageQuantity'])
-       nf_lev_order_type = users_d['users']['suma']['lev_nf']['lev_order_type']
-       nf_exchange = users_d['users']['suma']['lev_nf']['exchange']
-       nf_slm_order_type = users_d['users']['suma']['lev_nf']['slmOrderType']
-       nf_ref_pos_quantity = users_d['users']['suma']['lev_nf']['ref_pos_quantity']
-       # code block to figure out whether access token is available or not.
-
-       try:
-              if modified_date != datetime.date.today().__str__():
-                     access_token = AliceBlue.login_and_get_access_token(username=username, password=password,
-                                                                         twoFA=twofa,
-                                                                         api_secret=api_secret)
-                     logging.info("Generating a new access token {} and modifying generation date to {}"
-                                  .format(access_token, datetime.date.today()))
-              logging.debug("checking whether access token is valid")
-              alice = AliceBlue(username=username, password=password, access_token=access_token)
-       except Exception as e:
-              logging.error(" current token {} is invalid and and exception "
-                            "occurred in generating a new one ".format(access_token, e))
-              logging.debug("Invalid access token , generating a new access token")
-              access_token = AliceBlue.login_and_get_access_token(username=username, password=password, twoFA=twofa,
-                                                                  api_secret=api_secret)
-              alice = AliceBlue(username=username, password=password, access_token=access_token)
-              logging.info("Stored access token is invalid , generated a new access token {} ".format(access_token))
-    #   return alice.get_order_history()
+    try:
+        if modified_date != datetime.date.today().__str__():
+            access_token = AliceBlue.login_and_get_access_token(username=username, password=password,
+                                                                twoFA=twofa,
+                                                                api_secret=api_secret)
+            logging.info("Generating a new access token {} and modifying generation date to {}"
+                         .format(access_token, datetime.date.today()))
+        logging.debug("checking whether access token is valid")
+        alice = AliceBlue(username=username, password=password, access_token=access_token)
+    except Exception as e:
+        logging.error(" current token {} is invalid and and exception "
+                      "occurred in generating a new one ".format(access_token, e))
+        logging.debug("Invalid access token , generating a new access token")
+        access_token = AliceBlue.login_and_get_access_token(username=username, password=password, twoFA=twofa,
+                                                            api_secret=api_secret)
+        alice = AliceBlue(username=username, password=password, access_token=access_token)
+        logging.info("Stored access token is invalid , generated a new access token {} ".format(access_token))
     try:
         balance = alice.get_balance()
         if balance['status'] == 'success':
@@ -84,14 +83,14 @@ def placelev():
                 json.dump(users_d, f)
         req_data = request.get_json()
         if req_data:
-            instrument_token = req_data['instrument_token'];
-            exchange = req_data['exchange'];
-            trading_symbol = req_data['trading_symbol'];
+            instrument_token = req_data['instrument_token']
+            exchange = req_data['exchange']
+            trading_symbol = req_data['trading_symbol']
             if nf_non_trading_symbol not in trading_symbol:
                 lev_quantity = nf_leverage_quantity
             elif nf_non_trading_symbol in trading_symbol:
                 lev_quantity = bnf_leverage_quantity
-            lev_order_type_s = req_data['lev_order_type'];
+            lev_order_type_s = req_data['lev_order_type']
             if lev_order_type_s == 'SELL':
                 lev_order_type = TransactionType.Sell
             elif lev_order_type_s == 'BUY':
@@ -118,8 +117,8 @@ def placelev():
                 return response
     except Exception as e:
         logging.error("Exception in executing Penguin Cave and Smoke in Black {}".format(e))
-    return render_template('homepage.html')
+    return "error occurred"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
